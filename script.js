a08ee2992cc72721caac9490f754ca457bf8004c
GAURAV KUMAR <69577099+gks0o7@users.noreply.github.com>
2022-06-25 09:52:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-localStorage.clear();
+
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
